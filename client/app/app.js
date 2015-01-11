d411344844929b65ef866c73674081f1ae44130a
wzaghal <wisamzaghal@gmail.com>
2015-01-11 15:29:29 -0500

    add home ctrl

--- client/app/app.js
+++ client/app/app.js
@@ -1,2 +1,17 @@
 'use strict';
-angular.module('MyApp', ['ui.router']);
+angular.module('musicMatcher', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        //Routes
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/home/home.html',
+                controller: 'HomeCtrl as homeCtrl'
+            });
+
+
+
+    });
